c61978bc2ff1ba2711d192d50b26770eae260165
Eden Syoum <edensyoum@gmail.com>
2017-07-23 19:29:52 -0400

    Further configure menu component

--- src/Navigation/Button.js
+++ src/Navigation/Button.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import FontAwesome from 'react-fontawesome';
+import MotionMenu from '../../src';
 
 export default class NavigationButton extends Component {
   render() {
-    <div className="menu">
-      <p><FontAwesome name='bars' size='2x' /></p>
-    </div>
-}
+    return (
+      <MotionMenu
+        type="circle"
+        margin={120}>
+
+        <div className="menu">
+          <p><FontAwesome name='bars' size='2x' /></p>
+
+          <p><FontAwesome name='cogs' size='2x' /></p>
+        </div>
+
+      </MotionMenu>
+    )
+  };
+};
--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import NavigationButton from './Button';
+import NavigationMenu from './Menu';
 
 export default class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { opened: false };
+  }
+  showMenu() {
+    this.setState({ opened: !this.state.opened });
+  }
   render() {
     if (this.state.opened) {
       return <NavigationMenu />;
     } else {
-      return <NavigationButton />;
+      return <NavigationButton onClick={this.showMenu.bind(this)} />;
     }
   }
 }
