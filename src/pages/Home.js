287f9555086ce08bd7344d1d7a15416ad0d285f1
Eden Syoum <edensyoum@gmail.com>
2017-07-26 20:33:23 -0400

    Finishing for the day

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
 import Page from '../templates/Page';
+import Shapes from '../assets/img/shapes.svg';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 
 export default class Home extends Component {
   render() {
     return (
       <Page title="Home">
-
         <Grid>
           <Row>
             <Col xs={12}>
-              <Row center="xs">
-                <Col xs className="box2" />
+              <Row>
+                <Col xs>
+                  <img src={Shapes} className="box2" alt="design & development" />
+                </Col>
                 <Col xs className="devdesign">
                     <h1>eden syoum</h1>
-                    <h3>web development + design</h3>
+                    <h3>web development  +  design</h3>
                 </Col>
               </Row>
             </Col>
           </Row>
         </Grid>
-
       </Page>
     );
   }
