27b9b96ee6c9cd837f1063787b1289a7750008f0
Eden Syoum <edensyoum@gmail.com>
2017-08-01 14:33:20 -0400

    Begin styling work page

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Content from '../components/Content';
 import Parallax from 'react-springy-parallax';
+import Noise from '../assets/img/noise.svg';
 
 export default class Work extends Component {
   render() {
@@ -11,16 +12,23 @@ export default class Work extends Component {
           <Parallax ref='parallax' pages={3}>
 
             <Parallax.Layer
-              offset={.5}
-              speed={0.5}
-              style={{ backgroundColor: 'rgb(64,30,124)' }}>
+              offset={.17}
+              speed={2}>
 
-              <div>Work</div>
+              <img src={Noise} alt='Work' />
 
             </Parallax.Layer>
 
+          <Parallax.Layer
+            offset={.10}
+            speed={.25}>
+
+            <div className='work-header'>work</div>
+
+          </Parallax.Layer>
+
             <Parallax.Layer
-              offset={1}
+              offset={.43}
               speed={.5}
               style={{ backgroundColor: 'blue' }} />
 
