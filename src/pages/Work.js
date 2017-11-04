7957a9657c21ca891e7baceb6ab7ffe9c268756c
Eden Syoum <edensyoum@gmail.com>
2017-11-03 21:28:47 -0400

    Add and test react-tilt component

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -3,12 +3,17 @@ import { Grid, Row, Col } from 'react-flexbox-grid';
 import ProjectPreview from '../components/ProjectPreview';
 import Projects from '../projects.json';
 import Parallax from 'react-springy-parallax';
+import Tilt from 'react-tilt';
+
 
 export default class Work extends Component {
+  onMouseLeave(e) {
+    console.log('clientY:', e.nativeEvent.clientY);
+  }
   get projects() {
     let projectIDs = Object.keys(Projects);
 
-    return projectIDs.map(function(id) {
+    return projectIDs.map((id) => {
       let projectAttributes = Projects[id];
       let boxClassName = `projectcard projectcard--${id}`;
       let indexInArray = projectIDs.indexOf(id);
@@ -20,7 +25,20 @@ export default class Work extends Component {
           <Grid fluid>
             <Row center="xs">
               <Col bottom="xs">
-                <div className={`${boxClassName}`}/>
+
+                <div>
+                  <Tilt className="Tilt" options={{
+                      reverse : true,
+                      max : 5,
+                      perspective: 2000,
+                      axis : null,
+                      speed : 700
+                    }}  onMouseLeave={this.onMouseLeave}>
+                    <div className="Tilt-inner">
+                      <div className={`${boxClassName}`}/>
+                    </div>
+                  </Tilt>
+                </div>
 
                 <ProjectPreview index={indexInArray} {...projectAttributes} />
               </Col>
@@ -33,14 +51,16 @@ export default class Work extends Component {
 
   render() {
     return (
-      <Parallax ref="parallax" pages={5}>
+      <Parallax ref="parallax" pages={6}>
 
           <div className="work__wrapper">
             <div className="work__header">
               <div className="work__header--title">Featured Projects</div>
             </div>
 
+
             {this.projects}
+
 
         </div>
       </Parallax>
