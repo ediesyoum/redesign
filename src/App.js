4a1c0bbe2ec5a5cdd21faf3dca26e3465b85a0fe
Eden Syoum <edensyoum@gmail.com>
2017-11-05 18:12:09 -0500

    react router transition has a recent issue. removing from project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './styles/App.scss';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { spring, AnimatedRoute } from 'react-router-transition';
 import Home from './pages/Home';
 import About from './pages/About';
 import Work from './pages/Work';
@@ -9,25 +8,20 @@ import Contact from './pages/Contact';
 import ProjectPage from './components/ProjectPage';
 import Navigation from './components/Navigation';
 
-
 class App extends Component {
   render() {
     return(
       <div>
         <Navigation/>
-
-            <AnimatedSwitch
-              atEnter={bounceTransition.atEnter}
-              atLeave={bounceTransition.atLeave}
-              atActive={bounceTransition.atActive}
-              mapStyles={mapStyles}>
-              <Route exact path="/" component={Home} />
-              <Route path="/about" component={About} />
-              <Route path="/work" component={Work} />
-              <Route path="/contact" component={Contact} />
-              <Route path="/work/:id" component={ProjectPage} />
-            </AnimatedSwitch>
-
+        <Router>
+          <div>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route exact path="/work" component={Work} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/work/:id" component={ProjectPage} />
+          </div>
+        </Router>
       </div>
     );
   }
