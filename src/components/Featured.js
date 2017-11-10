e0f0f24b390e7262fc197e0ab04bec77c4c437b5
Eden Syoum <edensyoum@gmail.com>
2017-11-10 18:08:15 -0500

    Mobile styling of featured component

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -4,6 +4,7 @@ import Preview from '../components/Preview';
 import Projects from '../projects.json';
 import FaChevronCircleDown from 'react-icons/lib/fa/chevron-circle-down';
 import Footer from '../components/Footer';
+import MediaQuery from 'react-responsive';
 
 
 export default class Featured extends Component {
@@ -11,51 +12,94 @@ export default class Featured extends Component {
     let jotit = Projects['jot-it'];
     let csm = Projects['csm'];
     return (
-        <Parallax ref="parallax" pages={1.479}               speed={.5} className={'featured-wrapper scrollbar'}>
+        <Parallax ref="parallax" pages={1.448}               speed={.5} className={'featured-wrapper scrollbar'}>
 
-          <section className="featured">
+            <MediaQuery query="(min-device-width: 500px)" className="featured-preview-wrapper">
 
-            <Parallax.Layer
-              offset={0}
-              speed={.5}
-              className={'featured-bg--content'}>
+              <section className="featured">
 
-              <p>Featured Projects</p>
+                <Parallax.Layer
+                  offset={0}
+                  speed={.5}
+                  className={'featured-bg--content'}>
+
+                  <p>Featured Projects</p>
+
+                  <Parallax.Layer
+
+                    onClick={() => this.refs.parallax.scrollTo(0.211)}>
+                    <FaChevronCircleDown className="chevron-down"/>
+                  </Parallax.Layer>
+
+                </Parallax.Layer>
+              </section>
 
               <Parallax.Layer
-                style={{ height: 300 }}
-                onClick={() => this.refs.parallax.scrollTo(0.211)}>
-                <FaChevronCircleDown className="chevron-down"/>
+                offset={0.3}
+                speed={.5}
+                className={'feature01'}>
+                <Preview {...jotit} />
               </Parallax.Layer>
 
-            </Parallax.Layer>
-          </section>
+              <Parallax.Layer
+                offset={0.394}
+                speed={0.5}
+                className={'feature02'}>
+                <Preview {...csm} />
+              </Parallax.Layer>
 
-          <div className="featured-preview-wrapper">
-            <Parallax.Layer
-              offset={0.3}
-              speed={.5}
-              className={'feature01'}>
-              <Preview {...jotit} />
-            </Parallax.Layer>
+              <Parallax.Layer
+                offset={.488}
+                speed={0.5}
+                className={'feature03'}>
+                <Preview {...csm} />
+              </Parallax.Layer>
+            </MediaQuery>
 
-            <Parallax.Layer
-              offset={0.394}
-              speed={0.5}
-              className={'feature02'}>
-              <Preview {...csm} />
-            </Parallax.Layer>
+            <MediaQuery query="(max-device-width: 500px)" className="featured-preview-wrapper--sm">
+              <section className="featured">
 
-            <Parallax.Layer
-              offset={.488}
-              speed={0.5}
-              className={'feature03'}>
-              <Preview {...csm} />
-            </Parallax.Layer>
-          </div>
+                <Parallax.Layer
+                  offset={0}
+                  speed={.5}
+                  className={'featured-bg--content'}>
+
+                  <p>Featured Projects</p>
+
+                  <Parallax.Layer
+
+                    onClick={() => this.refs.parallax.scrollTo(0.211)}>
+                    <FaChevronCircleDown className="chevron-down"/>
+                  </Parallax.Layer>
+
+                </Parallax.Layer>
+              </section>
+
+
+              <Parallax.Layer
+                offset={0.306}
+                speed={.5}
+                className={'feature01--sm'}>
+                <Preview {...jotit} />
+              </Parallax.Layer>
+
+              <Parallax.Layer
+                offset={0.394}
+                speed={0.5}
+                className={'feature02--sm'}>
+                <Preview {...csm} />
+              </Parallax.Layer>
+
+              <Parallax.Layer
+                offset={.488}
+                speed={0.5}
+                className={'feature03--sm'}>
+                <Preview {...csm} />
+              </Parallax.Layer>
+            </MediaQuery>
 
           <Parallax.Layer
-            offset={.9}
+            offset={.75}
             speed={0.5}
             style={{ height: 6 }}>
             <Footer/>
