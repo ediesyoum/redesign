3042ccb4151234ca46d8514ffbf2dcb99f9dae72
Eden Syoum <edensyoum@gmail.com>
2017-11-21 20:08:48 -0500

    Continue work on project pg

--- src/components/Project.js
+++ src/components/Project.js
@@ -49,10 +49,10 @@ export default class Project extends Component {
                 <img className={this.headerClassName} alt="Test"/>
               </Parallax.Layer>
 
-              <Parallax.Layer offset={.5} speed={.1} style={{ backgroundColor: '#FECC52' }} />
-              <Parallax.Layer offset={1.3} speed={.1} style={{ backgroundColor: '#805E73' }} />
-              <Parallax.Layer offset={2} speed={.1} style={{ backgroundColor: '#87BCDE' }} />
-              <Parallax.Layer offset={3} speed={.1} style={{ backgroundColor: '#6b1586' }} />
+              <Parallax.Layer offset={.5} speed={.1} style={{ height: 1200, backgroundColor: '#FECC52' }} />
+              <Parallax.Layer offset={1.5} speed={.1} style={{ height: 1200, backgroundColor: '#805E73' }} />
+              <Parallax.Layer offset={2.9} speed={.1} style={{ height: 1200, backgroundColor: '#87BCDE' }} />
+              <Parallax.Layer offset={3.8} speed={.1} style={{ height: 1200, backgroundColor: '#6b1586' }} />
 
               <div className="project__header">
                 <div className="project__header--title">
@@ -67,11 +67,21 @@ export default class Project extends Component {
 
                 <div className="project__summary">
 
-                  <div className="project-section-title__background">
-                    <h3>Project Summary</h3>
-                  </div>
+                  <Grid fluid>
+                    <Row>
+                      <Col xsOffset={0} xs={10}>
+                        <h3 className="project-section-title__background">Project Description</h3>
+                        <div><p className="project_desc">{this.props.summary}</p></div>
+                      </Col>
 
-                  <div><p>{this.props.summary}</p></div>
+                      <Col xs="end">
+                        <h3>Test</h3>
+                      </Col>
+                    </Row>
+                  </Grid>
+
+
+                  <div className="slanted-bg"/>
 
                   <div className="image--perspective">
                     <img className={this.desktopProjectImgClassName} src={this.projectImage('desktop')} alt={`${this.props.id} Desktop Project`} title={`${this.props.id} Desktop Project`} />
@@ -81,29 +91,53 @@ export default class Project extends Component {
                   <div className="project-section-title__background">
                     <h3>Technical Details</h3>
                   </div>
-                  <p>{this.props.technical}</p>
+                  <p className="project__technical">{this.props.technical}</p>
 
 
-                  <section className="project__links">
-                    <ProjectLinks links={this.props.links} />
-                    <div><img className={this.tabletProjectImgClassName} src={this.projectImage('tablet')} alt={`${this.props.id} Tablet Project`} title={`${this.props.id} Tablet Project`}/></div>
-                  </section>
-
-                  <img className={this.mobileProjectImgClassName} alt={`${this.props.id} Mobile Project`} title={`${this.props.id} Mobile Project`} src={this.projectImage('mobile')} />
-
-
-                  <div className="project__tag--wrapper">
-                    <section className="project__tags">
-                      {this.tags}
-                    </section>
-                  </div>
-
-                  <nav className="project__navigation">
-                    <ProjectNavigationLink id={this.props.id} rel="previous" />
-                    <ProjectNavigationLink id={this.props.id} rel="next" />
-                  </nav>
+                  <Grid fluid>
+                    <Row>
+                      <Col xsOffset={0} xs={8}>
+                        <div className="image--perspective">
+                          <img className={this.mobileProjectImgClassName} alt={`${this.props.id} Mobile Project`} title={`${this.props.id} Mobile Project`} src={this.projectImage('mobile')} />
+                        </div>
+                      </Col>
+                      <Col xsOffset={9} xs={3}>
+                        <div className="project__tag--wrapper">
+                          <section className="project__tags">
+                            {this.tags}
+                          </section>
+                        </div>
+                      </Col>
+                    </Row>
+                  </Grid>
                 </div>
               </ScrollAnimation>
+
+              <div className="slanted-bg"/>
+
+              <section className="project__links">
+                <Grid fluid>
+                  <Row>
+                    <Col xsoffSet={0} xs={3}>
+                      <ProjectLinks links={this.props.links} />
+                    </Col>
+
+                    <Col xs="end">
+                      <div className="image--perspective">
+                        <img className={this.tabletProjectImgClassName} src={this.projectImage('tablet')} alt={`${this.props.id} Tablet Project`} title={`${this.props.id} Tablet Project`}/>
+                      </div>
+                    </Col>
+
+                  </Row>
+                </Grid>
+              </section>
+
+              <nav className="project__navigation">
+                <ProjectNavigationLink id={this.props.id} rel="previous" />
+                <ProjectNavigationLink id={this.props.id} rel="next" />
+              </nav>
+
+
               <section className="footer">
                 <Grid fluid>
                   <Row>
