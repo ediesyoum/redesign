3f5d4d12eb6ef047d90e81396b30dc67dcb16717
Eden Syoum <edensyoum@gmail.com>
2017-11-23 11:59:45 -0500

    Add additional changes

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import FaAngleDoubleRight from 'react-icons/lib/fa/angle-double-right';
 
 export default class Preview extends Component {
   get route() {
     return `/work/${this.props.id}`;
   }
   get projectFeatureLinkClassName() {
-    return `${this.props.id}___feature--link`;
+    return `featured__link featured__link--${this.props.id}`;
   }
 
   render() {
@@ -14,6 +15,7 @@ export default class Preview extends Component {
       <Link to={this.route}>
         <div className={this.projectFeatureLinkClassName}>
           {this.props.name}
+          <FaAngleDoubleRight />
         </div>
       </Link>
     );
--- src/pages/About.js
+++ src/pages/About.js
@@ -4,8 +4,16 @@ import MediaQuery from 'react-responsive';
 import { Link } from 'react-router-dom';
 import Eyes from '../assets/img/eyes.jpg';
 import { Grid, Col, Row } from 'react-flexbox-grid';
+import 'animate.css/animate.min.css';
 
 export default class About extends Component {
+  seeBackground(event) {
+    let background = document.getElementById('background');
+    background.classList.add('bounceInUp');
+    background.style.display = 'block';
+    return false;
+  }
+
   render() {
     return (
           <div>
@@ -21,13 +29,15 @@ export default class About extends Component {
 
                     <p>Throughout my technical career, I have worked hard to be a user advocate.  As an IT consultant turned web developer, I have spent several years studying user habits, seeing their pain points first hand, working tirelessly to resolve and prevent future obstacles. This is why designing with consideration and empathy for the user’s experience comes naturally to me - it empowers me to create visually engaging, functional and aesthetic products that humans will truly love to use, and use often.</p>
 
-                    <a href="/">See Background</a>
+                    <a href="#" onClick={this.seeBackground}>See Background</a>
 
-                    <h3>Background</h3>
+                    <div id="background" className="about__background">
+                      <h3>Background</h3>
 
-                    <p>Creating graphics and simple HTML and CSS snippets became a hobby for me around the age of 12. It was at this time that web design became the center of my world - I would go on to build websites and blogs, host domains and design beautiful user interfaces, brands, logos and complex graphics of all kinds. I did web design for six years until I went to college, where I studied political and social sciences. Following graduation, I sought out technical work where I could design, code AND help everyday people to better use tech to their advantage, and I eventually found this in doing web design and working as an IT consultant at the University of Pennsylvania.</p>
+                      <p>Creating graphics and simple HTML and CSS snippets became a hobby for me around the age of 12. It was at this time that web design became the center of my world - I would go on to build websites and blogs, host domains and design beautiful user interfaces, brands, logos and complex graphics of all kinds. I did web design for six years until I went to college, where I studied political and social sciences. Following graduation, I sought out technical work where I could design, code AND help everyday people to better use tech to their advantage, and I eventually found this in doing web design and working as an IT consultant at the University of Pennsylvania.</p>
 
-                    <p>Flash forward to 2014, and major advances in JavaScript and mobile development and design have completely revamped the state of the web. For the past three years, I have worked to gain experience with a variety of <Link to="/work">projects</Link>, delving deeper into my lifelong love of code and well-implemented UI/UX design. As of recent, I have become very interested in ReactJS for its ease of usability via components, and how powerful and portable it is as an front end tool for interactive UX design. <Link to="/work/portfolio-current">See my case study on my first ReactJS project.</Link></p>
+                      <p>Flash forward to 2014, and major advances in JavaScript and mobile development and design have completely revamped the state of the web. For the past three years, I have worked to gain experience with a variety of <Link to="/work">projects</Link>, delving deeper into my lifelong love of code and well-implemented UI/UX design. As of recent, I have become very interested in ReactJS for its ease of usability via components, and how powerful and portable it is as an front end tool for interactive UX design. <Link to="/work/portfolio-current">See my case study on my first ReactJS project.</Link></p>
+                    </div>
                   </section>
                 </main>
                 <section className="footer">
@@ -61,13 +71,15 @@ export default class About extends Component {
 
                     <p>Throughout my technical career, I have worked hard to be a user advocate.  As an IT consultant turned web developer, I have spent several years studying user habits, seeing their pain points first hand, working tirelessly to resolve and prevent future obstacles. This is why designing with consideration and empathy for the user’s experience comes naturally to me - it empowers me to create visually engaging, functional and aesthetic products that humans will truly love to use, and use often.</p>
 
-                    <a href="/">See Background</a>
+                    <a href="#" onClick={this.seeBackground}>See Background</a>
 
-                    <h3>Background</h3>
+                    <div id="background" className="about__background animated">
+                      <h3>Background</h3>
 
-                    <p>Creating graphics and simple HTML and CSS snippets became a hobby for me around the age of 12. It was at this time that web design became the center of my world - I would go on to build websites and blogs, host domains and design beautiful user interfaces, brands, logos and complex graphics of all kinds. I did web design for six years until I went to college, where I studied political and social sciences. Following graduation, I sought out technical work where I could design, code AND help everyday people to better use tech to their advantage, and I eventually found this in doing web design and working as an IT consultant at the University of Pennsylvania.</p>
+                      <p>Creating graphics and simple HTML and CSS snippets became a hobby for me around the age of 12. It was at this time that web design became the center of my world - I would go on to build websites and blogs, host domains and design beautiful user interfaces, brands, logos and complex graphics of all kinds. I did web design for six years until I went to college, where I studied political and social sciences. Following graduation, I sought out technical work where I could design, code AND help everyday people to better use tech to their advantage, and I eventually found this in doing web design and working as an IT consultant at the University of Pennsylvania.</p>
 
-                    <p>Flash forward to 2014, and major advances in JavaScript and mobile development and design have completely revamped the state of the web. For the past three years, I have worked to gain experience with a variety of <Link to="/work">projects</Link>, delving deeper into my lifelong love of code and well-implemented UI/UX design. As of recent, I have become very interested in ReactJS for its ease of usability via components, and how powerful and portable it is as an front end tool for interactive UX design. <Link to="/work/portfolio-current">See my case study on my first ReactJS project.</Link></p>
+                      <p>Flash forward to 2014, and major advances in JavaScript and mobile development and design have completely revamped the state of the web. For the past three years, I have worked to gain experience with a variety of <Link to="/work">projects</Link>, delving deeper into my lifelong love of code and well-implemented UI/UX design. As of recent, I have become very interested in ReactJS for its ease of usability via components, and how powerful and portable it is as an front end tool for interactive UX design. <Link to="/work/portfolio-current">See my case study on my first ReactJS project.</Link></p>
+                    </div>
                   </section>
                 </main>
                 <section className="footer">
